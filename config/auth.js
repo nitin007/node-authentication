1f935b46f8fa304c852e8c06e89e3d2ecad0553f
Nitin Gupta <nitin.gupta@vinsol.com>
2014-05-27 19:30:23 +0530

    first commit

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '521516167974004', // your App ID
+		'clientSecret' 	: 'c9f6fc868b59296855d4219edd31219f', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'QcaZRMEVi5DeojZ5yaDeOoEDz',
+		'consumerSecret' 	: 'WP5x2oGFgkCuc2Ye76kOj2goIBE8zHfm877nxw8MaxKwpGTdUS',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '134939358493-sekadtpqo7d2evouhlok7of9im8tsgag.apps.googleusercontent.com',
+		'clientSecret' 	: 'B25t0tTjkg3EDPCBM_ApXZeQ',
+		'callbackURL' 	: 'http://127.0.0.1:8080/auth/google/callback'
+	}
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'url' : 'mongodb://nodeuser:nodified@ds033419.mongolab.com:33419/node-authentication' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  // mongodb://nodeuser:nodified@ds033419.mongolab.com:33419/node-authentication
+  // mongodb://node:nodefied@novus.modulusmongo.net:27017/Y5remema
+};
